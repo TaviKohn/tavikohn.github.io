15b76fb9e492062c699bb501eb1dde603d675888
Tavi Kohn <XshadowlynxX@gmail.com>
2016-10-03 21:37:27 -0600

    Switched to npm instead of bower, and set up babel JSX compile

---
+++ build/js/app.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Tavi on 10/3/16.
+ */
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+ReactDOM.render(React.createElement(
+  'div',
+  null,
+  'Hello World'
+), document.getElementById('app'));
